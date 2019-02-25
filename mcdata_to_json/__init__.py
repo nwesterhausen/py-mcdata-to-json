7acba4551a79385e00c94fe0c3040a91308c0e58
Nicholas Westerhausen <nwesterhausen@gmail.com>
2019-02-25 16:28:28 -0500

    Logging removed, added print to main

--- mcdata_to_json/__init__.py
+++ mcdata_to_json/__init__.py
@@ -1,2 +1,1 @@
 """Python library to turn minecraft server data into servable json files"""
-LOGGER_NAME = 'mcdata_to_json'
--- mcdata_to_json/__main__.py
+++ mcdata_to_json/__main__.py
@@ -10,10 +10,6 @@ import mcdata_to_json.configuration as Config
 import mcdata_to_json.mojang_api as mojang_api
 from mcdata_to_json.mcdata import data_extractor, advancements
 import mcdata_to_json.player as player
-from mcdata_to_json import LOGGER_NAME
-
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-_LOGGER = logging.getLogger(name=LOGGER_NAME)
 
 Config.validatePaths()
 
@@ -23,16 +19,20 @@ if (Config.PLAYERDATA_DIR not in Config.NONEXISTENT_FILES):
         map(lambda fn: fn.replace('.dat', ''),
             os.listdir(Config.PLAYERDATA_DIR)))
 
-_LOGGER.info("Found {} players".format(len(UUIDS)))
 for uuid in UUIDS:
     player.save_temp_playerdata_json(uuid)
     player.save_temp_advancement_json(uuid)
     player.save_temp_stats_json(uuid)
 
+print('Extracting possible advancements from minecraft server.jar')
 data_extractor.extract_server_jar_assets()
+
 advancements.cache_possible_advancements()
+print('Exporting server advancements progress')
 advancements.save_completed_advancements(UUIDS.copy())
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(mojang_api.save_cache_mojang_profiles(UUIDS.copy()))
 loop.close()
+
+print(f'Exporting combined JSON for {len(UUIDS)} players.')
