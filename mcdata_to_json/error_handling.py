aafff730d265b714af26521bb4342340b789f7b1
Nicholas Westerhausen <nwesterhausen@gmail.com>
2019-02-25 14:01:36 -0500

    replaced logging in api functions

--- mcdata_to_json/error_handling.py
+++ mcdata_to_json/error_handling.py
@@ -1,14 +1,30 @@
 class Error(Exception):
-    """Base class for exceptions in this module."""
-    pass
-
-
-class MissingRequiredFileError(Error):
-    """Exception raised for missing a required file.
+    """Base class for exceptions in this module.
 
     Attributes:
-        message -- explanation of the error
-    """
+        message -- explanation of the error"""
 
     def __init__(self, message):
         self.message = message
+
+
+class MissingRequiredFileError(Error):
+    """Exception raised for missing a required file."""
+    pass
+
+
+class MojangAPIError(Error):
+    """Exception raised when interacting with the Mojang API"""
+    pass
+
+
+class Non200ResponseError(MojangAPIError):
+    """Exception raised when receiving a non-OK (2xx) response"""
+    pass
+
+
+class InvalidQueryError(MojangAPIError):
+    """Exception raised when the Mojang API returns an error
+
+    This can happen if you are rate-limited as well."""
+    pass
--- mcdata_to_json/mojang_api.py
+++ mcdata_to_json/mojang_api.py
@@ -7,9 +7,7 @@ import base64
 import time
 import typing
 import mcdata_to_json.configuration as Config
-from mcdata_to_json import LOGGER_NAME
-
-_LOGGER = logging.getLogger(name=LOGGER_NAME)
+import mcdata_to_json.error_handling as errors
 
 PROFILE_API_BASE = 'https://sessionserver.mojang.com/session/minecraft/profile'
 CONSIDERED_CACHED = 1000 * 60 * 60 * 4  # 4 hours
@@ -24,13 +22,11 @@ def uri_decode_uuid(uuid: str) -> str:
         [uuid[0, 8], uuid[8, 12], uuid[12, 16], uuid[16, 20], uuid[20]])
 
 
-async def save_cache_mojang_profiles(uuids: typing.List):
+async def save_cache_mojang_profiles(uuids: typing.List) -> None:
     for file in os.listdir(Config.TEMP_PROFILE_JSON_DIR):
         if time.time() - os.path.getmtime(
                 os.path.join(Config.TEMP_PROFILE_JSON_DIR,
                              file)) < CONSIDERED_CACHED:
-            _LOGGER.info("Cached profile for {} is recent enough.".format(
-                file.replace('.json', '')))
             uuids.pop(uuids.index(file.replace('.json', '')))
     # Age check here as well?
     async with aiohttp.ClientSession() as session:
@@ -45,12 +41,12 @@ async def save_cache_mojang_profiles(uuids: typing.List):
 async def get_mojang_profile(uuid: str,
                              session: aiohttp.ClientSession) -> typing.Dict:
     apiTarget = "{}/{}".format(PROFILE_API_BASE, uri_encode_uuid(uuid))
-    emptyResponse = {}
+    emptyResponse: dict = {}
     async with session.get(apiTarget) as resp:
         respjson = await resp.json()
         if resp.status == 200:
             if 'error' in respjson:
-                _LOGGER.warn(json.dumps(respjson))
+                errors.InvalidQueryError(json.dumps(respjson))
             else:
                 for property in respjson["properties"]:
                     b64_value = property["value"]
@@ -59,6 +55,7 @@ async def get_mojang_profile(uuid: str,
                         base64.decodebytes(str.encode(b64_value)))
                 return respjson
         else:
-            _LOGGER.warn("Unable to get profile for {} due to {}:{}".format(
-                uuid, resp.status, resp.reason))
+            errors.Non200ResponseError(
+                "Unable to get profile for {} due to {}:{}".format(
+                    uuid, resp.status, resp.reason))
         return emptyResponse
