f42853e584183efd9796cedafaa31522be1a43f1
Nicholas Westerhausen <nwesterhausen@gmail.com>
2019-02-27 15:31:39 -0500

    Now is aware of chunk-level changes in region files and skips or updates appropriately

--- mcdata_to_json/mca_parser.py
+++ mcdata_to_json/mca_parser.py
@@ -9,47 +9,85 @@ import mcdata_to_json.configuration as Config
 from mcdata_to_json.mcdata.mca_reader import Mca
 from mcdata_to_json.tools import dict_merge
 
-UNQUOTED_KEY_RE = re.compile("(\w+): ")
-NUMBER_TYPE_RE = re.compile("([0-9][0-9.]*)(b|f|L|d|s)")
-LIST_TYPE_RE = re.compile("\[(I);")
+UNQUOTED_KEY_RE = re.compile(r"(\w+): ")
+NUMBER_TYPE_RE = re.compile(r"([0-9][0-9.]*)(b|f|L|d|s|i|B)")
+LIST_TYPE_RE = re.compile(r"\[(B|F|L|D|S|I);")
+
+EMPTY_CHUNK = {
+    'TileEntities': [],
+    'Entities': [],
+    'Timestamp': None,
+    'Status': 'empty',
+    'Structures': {},
+    'xPos': None,
+    'zPos': None,
+    'InhabitedTime': None,
+    'LastUpdate': None,
+    'Biomes': []
+}
 
 
 def cache_json_for_region_files():
     """Saves json for each region file into the data cache"""
     for filename in os.listdir(Config.OVERWORLD_REGION_DIR):
-        cacheFilepath = os.path.join(Config.CACHED_MCA_JSON_DIR,
-                                     filename.replace('.mca', '.json'))
-        rawFilepath = os.path.join(Config.OVERWORLD_REGION_DIR, filename)
-        if not os.path.exists(cacheFilepath) or os.path.getmtime(
-                cacheFilepath) < os.path.getmtime(rawFilepath):
-            cache_nbtjson_for_region(
-                os.path.join(Config.OVERWORLD_REGION_DIR, filename))
+        cache_nbtjson_for_region(
+            os.path.join(Config.OVERWORLD_REGION_DIR, filename))
 
 
 def cache_nbtjson_for_region(regionfilepath):
     """Saves json for the provided region file (r.1.1.mca)"""
     region = os.path.basename(regionfilepath).replace('.mca', '')
+    cachepath = os.path.join(Config.CACHED_MCA_JSON_DIR, f'{region}.json')
     print(f'Working region {region}')
     mca = Mca(regionfilepath)
-    nbtjson: dict = {'TileEntities': [], 'Entities': []}
+    shouldCheckCached = False
+    nbtjson: dict = {'TileEntities': [], 'Entities': [], 'Chunks': {}}
+    updatedChunks = 0
+    if (os.path.exists(cachepath)):
+        shouldCheckCached = True
+        cachef = open(cachepath)
+        nbtjson = json.loads(cachef.read())
+        cachef.close()
     for i in range(32):
         for j in range(32):
+            if shouldCheckCached and f'x{i}z{j}' in nbtjson.keys():
+                if mca.get_timestamp(i,
+                                     j) == nbtjson[f'x{i}z{j}']['Timestamp']:
+                    sys.stdout.write(
+                        f'\rCache is up to date for {i}, {j}      ')
+                    sys.stdout.flush()
+                    continue
             chunkjson = nbtjson_for_chunk(mca, i, j)
+            updatedChunks += 1
             nbtjson['TileEntities'].extend(chunkjson['TileEntities'])
             nbtjson['Entities'].extend(chunkjson['Entities'])
+            nbtjson[f'x{i}z{j}'] = {
+                'Timestamp': mca.get_timestamp(i, j),
+                'Status': chunkjson['Status'],
+                'Structures': chunkjson['Structures'],
+                'xPos': chunkjson['xPos'],
+                'zPos': chunkjson['zPos'],
+                'InhabitedTime': chunkjson['InhabitedTime'],
+                'LastUpdate': chunkjson['LastUpdate'],
+                'Biomes': chunkjson['Biomes']
+            }
     save_cached_json(region, nbtjson)
+    print()
+    if updatedChunks > 0:
+        print(f'Had to update the cache for {updatedChunks} / {32*32} chunks.')
 
 
 def nbtjson_for_chunk(mcaobj, x, z):
     """Return json of the nbt for the chunk at x z"""
-    print(f'Working chunk {x}, {z}')
+    sys.stdout.write(f'\rWorking chunk {x}, {z}                      ')
+    sys.stdout.flush()
+    if mcaobj.get_timestamp(x, z) == 0:
+        return EMPTY_CHUNK
     region = mcaobj.get_data(x, z)
-    nbt = nbtlib.File.from_buffer(io.BytesIO(region))
-    chunkjson: dict = {'TileEntities': [], 'Entities': []}
-    if mcaobj.get_data_size(x, z) != 0:
-        if nbt.root_name != None:
-            chunkjson = recast_nbt_to_normals(nbt.root)
-    return chunkjson
+    nbt = nbtlib.File.parse(io.BytesIO(region))
+    if mcaobj.get_data_size(x, z) == 0 or nbt.root_name == None:
+        return EMPTY_CHUNK
+    return recast_nbt_to_normals(nbt.root)
 
 
 def save_cached_json(region, nbtjson):
@@ -60,25 +98,26 @@ def save_cached_json(region, nbtjson):
 
 
 def recast_nbt_to_normals(nbtroot: nbtlib.tag.Compound) -> dict:
-    if 'Level' in nbtroot.keys():
-        ents = nbtroot['Level']['Entities'].__str__()
-        ents = UNQUOTED_KEY_RE.sub(r'"\1": ', ents)
-        ents = NUMBER_TYPE_RE.sub(r'\1', ents)
-        ents = LIST_TYPE_RE.sub(r'', ents)
-        ents = ast.literal_eval(ents)
-        tents = nbtroot['Level']['TileEntities'].__str__()
-        tents = UNQUOTED_KEY_RE.sub(r'"\1": ', tents)
-        tents = NUMBER_TYPE_RE.sub(r'\1', tents)
-        tents = LIST_TYPE_RE.sub(r'[', tents)
-        tents = ast.literal_eval(tents)
-    else:
-        ents = []
-        tents = []
-    return {'Entities': ents, 'TileEntities': tents}
+    level = nbtroot['Level'].__str__()
+    level = UNQUOTED_KEY_RE.sub(r'"\1": ', level)
+    level = NUMBER_TYPE_RE.sub(r'\1', level)
+    level = LIST_TYPE_RE.sub(r'[', level)
+    level = ast.literal_eval(level)
+    return {
+        'xPos': level['xPos'],
+        'zPos': level['zPos'],
+        'InhabitedTime': level['InhabitedTime'],
+        'LastUpdate': level['LastUpdate'],
+        'Biomes': level['Biomes'],
+        'Status': level['Status'],
+        'Structures': level['Structures'],
+        'Entities': level['Entities'],
+        'TileEntities': level['TileEntities']
+    }
 
 
 if __name__ == '__main__':
     # cache_nbtjson_for_region(
-    #     os.path.join(Config.OVERWORLD_REGION_DIR, 'r.-1.5.mca'))
+    #     os.path.join(Config.OVERWORLD_REGION_DIR, 'r.-7.-2.mca'))
     m = Mca(os.path.join(Config.OVERWORLD_REGION_DIR, 'r.-1.-1.mca'))
-    print(nbtjson_for_chunk(m, 1, 9))
+    print(nbtjson_for_chunk(m, 8, 20))
---
+++ mcdata_to_json/mcdata/biome_dictionary.py
@@ -0,0 +1,302 @@
+biomes = {
+    '0': {
+        "id": "ocean",
+        "name": "Ocean"
+    },
+    '24': {
+        "id": "deep_ocean",
+        "name": "Deep Ocean"
+    },
+    '10': {
+        "id": "frozen_ocean",
+        "name": "Frozen Ocean"
+    },
+    '50': {
+        "id": "deep_frozen_ocean",
+        "name": "Deep Frozen Ocean"
+    },
+    '46': {
+        "id": "cold_ocean",
+        "name": "Cold Ocean"
+    },
+    '49': {
+        "id": "deep_cold_ocean",
+        "name": "Deep Cold Ocean"
+    },
+    '45': {
+        "id": "lukewarm_ocean",
+        "name": "Lukewarm Ocean"
+    },
+    '48': {
+        "id": "deep_lukewarm_ocean",
+        "name": "Deep Lukewarm Ocean"
+    },
+    '44': {
+        "id": "warm_ocean",
+        "name": "Warm Ocean"
+    },
+    '47': {
+        "id": "deep_warm_ocean",
+        "name": "Deep Warm Ocean"
+    },
+    '7': {
+        "id": "river",
+        "name": "River"
+    },
+    '11': {
+        "id": "frozen_river",
+        "name": "Frozen River"
+    },
+    '16': {
+        "id": "beach",
+        "name": "Beach"
+    },
+    '25': {
+        "id": "stone_shore",
+        "name": "Stone Shore"
+    },
+    '26': {
+        "id": "snowy_beach",
+        "name": "Snowy Beach"
+    },
+    '4': {
+        "id": "forest",
+        "name": "Forest"
+    },
+    '18': {
+        "id": "wooded_hills",
+        "name": "Wooded Hills"
+    },
+    '132': {
+        "id": "flower_forest",
+        "name": "Flower Forest"
+    },
+    '27': {
+        "id": "birch_forest",
+        "name": "Birch Forest"
+    },
+    '28': {
+        "id": "birch_forest_hills",
+        "name": "Birch Forest Hills"
+    },
+    '155': {
+        "id": "tall_birch_forest",
+        "name": "Tall Birch Forest"
+    },
+    '156': {
+        "id": "tall_birch_hills",
+        "name": "Tall Birch Hills"
+    },
+    '29': {
+        "id": "dark_forest",
+        "name": "Dark Forest"
+    },
+    '157': {
+        "id": "dark_forest_hills",
+        "name": "Dark Forest Hills"
+    },
+    '21': {
+        "id": "jungle",
+        "name": "Jungle"
+    },
+    '22': {
+        "id": "jungle_hills",
+        "name": "Jungle Hills"
+    },
+    '149': {
+        "id": "modified_jungle",
+        "name": "Modified Jungle"
+    },
+    '23': {
+        "id": "jungle_edge",
+        "name": "Jungle Edge"
+    },
+    '151': {
+        "id": "modified_jungle_edge",
+        "name": "Modified Jungle Edge"
+    },
+    '168‌': {
+        "id": "bamboo_jungle",
+        "name": "Bamboo Jungle‌ "
+    },
+    '169‌': {
+        "id": "bamboo_jungle_hills",
+        "name": "Bamboo Jungle Hills‌ "
+    },
+    '5': {
+        "id": "taiga",
+        "name": "Taiga"
+    },
+    '19': {
+        "id": "taiga_hills",
+        "name": "Taiga Hills"
+    },
+    '133': {
+        "id": "taiga_mountains",
+        "name": "Taiga Mountains"
+    },
+    '30': {
+        "id": "snowy_taiga",
+        "name": "Snowy Taiga"
+    },
+    '31': {
+        "id": "snowy_taiga_hills",
+        "name": "Snowy Taiga Hills"
+    },
+    '158': {
+        "id": "snowy_taiga_mountains",
+        "name": "Snowy Taiga Mountains"
+    },
+    '32': {
+        "id": "giant_tree_taiga",
+        "name": "Giant Tree Taiga"
+    },
+    '33': {
+        "id": "giant_tree_taiga_hills",
+        "name": "Giant Tree Taiga Hills"
+    },
+    '160': {
+        "id": "giant_spruce_taiga",
+        "name": "Giant Spruce Taiga"
+    },
+    '161': {
+        "id": "giant_spruce_taiga_hills",
+        "name": "Giant Spruce Taiga Hills"
+    },
+    '14': {
+        "id": "mushroom_fields",
+        "name": "Mushroom Fields"
+    },
+    '15': {
+        "id": "mushroom_field_shore",
+        "name": "Mushroom Field Shore"
+    },
+    '6': {
+        "id": "swamp",
+        "name": "Swamp"
+    },
+    '134': {
+        "id": "swamp_hills",
+        "name": "Swamp Hills"
+    },
+    '35': {
+        "id": "savanna",
+        "name": "Savanna"
+    },
+    '36': {
+        "id": "savanna_plateau",
+        "name": "Savanna Plateau"
+    },
+    '163': {
+        "id": "shattered_savanna",
+        "name": "Shattered Savanna"
+    },
+    '164': {
+        "id": "shattered_savanna_plateau",
+        "name": "Shattered Savanna Plateau"
+    },
+    '1': {
+        "id": "plains",
+        "name": "Plains"
+    },
+    '129': {
+        "id": "sunflower_plains",
+        "name": "Sunflower Plains"
+    },
+    '2': {
+        "id": "desert",
+        "name": "Desert"
+    },
+    '17': {
+        "id": "desert_hills",
+        "name": "Desert Hills"
+    },
+    '130': {
+        "id": "desert_lakes",
+        "name": "Desert Lakes"
+    },
+    '12': {
+        "id": "snowy_tundra",
+        "name": "Snowy Tundra"
+    },
+    '13': {
+        "id": "snowy_mountains",
+        "name": "Snowy Mountains"
+    },
+    '140': {
+        "id": "ice_spikes",
+        "name": "Ice Spikes"
+    },
+    '3': {
+        "id": "mountains",
+        "name": "Mountains"
+    },
+    '34': {
+        "id": "wooded_mountains",
+        "name": "Wooded Mountains"
+    },
+    '131': {
+        "id": "gravelly_mountains",
+        "name": "Gravelly Mountains"
+    },
+    '162': {
+        "id": "modified_gravelly_mountains",
+        "name": "Gravelly Mountains+"
+    },
+    '20': {
+        "id": "mountain_edge",
+        "name": "Mountain Edge"
+    },
+    '37': {
+        "id": "badlands",
+        "name": "Badlands"
+    },
+    '39': {
+        "id": "badlands_plateau",
+        "name": "Badlands Plateau"
+    },
+    '167': {
+        "id": "modified_badlands_plateau",
+        "name": "Modified Badlands Plateau"
+    },
+    '38': {
+        "id": "wooded_badlands_plateau",
+        "name": "Wooded Badlands Plateau"
+    },
+    '166': {
+        "id": "modified_wooded_badlands_plateau",
+        "name": "Modified Wooded Badlands Plateau"
+    },
+    '165': {
+        "id": "eroded_badlands",
+        "name": "Eroded Badlands"
+    },
+    '8': {
+        "id": "nether",
+        "name": "Nether"
+    },
+    '9': {
+        "id": "the_end",
+        "name": "The End"
+    },
+    '40': {
+        "id": "small_end_islands",
+        "name": "Small End Islands"
+    },
+    '41': {
+        "id": "end_midlands",
+        "name": "End Midlands"
+    },
+    '42': {
+        "id": "end_highlands",
+        "name": "End Highlands"
+    },
+    '43': {
+        "id": "end_barrens",
+        "name": "End Barrens"
+    },
+    '127': {
+        "id": "the_void",
+        "name": "The Void"
+    }
+}
