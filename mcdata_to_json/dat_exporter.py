f2f7b862bcad44b9ce231645871449725dbf5089
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2022-08-24 16:37:27 +0000

    fix: 🐛 fix nbt exporting
    when calling nbt[""] we were failing hard (issue #1) so I have changed the code to just export the entire nbt and that seems to work just fine. it isn't clear if this is going to make different JSON because I don't have old JSON to reference. Also added a few more sanity checks for files exiting during parsing.

--- mcdata_to_json/dat_exporter.py
+++ mcdata_to_json/dat_exporter.py
@@ -7,38 +7,45 @@ import mcdata_to_json.configuration as Config
 
 def export_level_dat() -> None:
     nbt = nbtlib.load(Config.LEVELDAT_FILE)
-    jsnbt = json.dumps(nbt[""])
+    jsnbt = json.dumps(nbt)
     with open(os.path.join(Config.OUTPUT_DIR, "leveldat.json"), 'w') as f:
         f.write(jsnbt)
 
 
 def export_raids_dat() -> None:
     raidsJson: dict = {}
-    nbt = nbtlib.load(os.path.join(Config.WORLD_DATA_DIR, 'raids.dat'))
-    raidsJson['overworld'] = nbt[""]
-    nbt = nbtlib.load(os.path.join(Config.NETHER_DATA_DIR, 'raids_nether.dat'))
-    raidsJson['nether'] = nbt[""]
-    nbt = nbtlib.load(os.path.join(Config.END_DATA_DIR, 'raids_end.dat'))
-    raidsJson['end'] = nbt[""]
+    if (Config.OVERWORLD_RAIDS_FILE not in Config.NONEXISTENT_FILES):
+        nbt = nbtlib.load(Config.OVERWORLD_RAIDS_FILE)
+        raidsJson['overworld'] = nbt
+    if (Config.NETHER_RAIDS_FILE not in Config.NONEXISTENT_FILES):
+        nbt = nbtlib.load(Config.NETHER_RAIDS_FILE)
+        raidsJson['nether'] = nbt
+    if (Config.END_RAIDS_FILE not in Config.NONEXISTENT_FILES):
+        nbt = nbtlib.load(Config.END_RAIDS_FILE)
+        raidsJson['end'] = nbt
     with open(os.path.join(Config.OUTPUT_DIR, 'raids.json'), 'w') as f:
         f.write(json.dumps(raidsJson))
 
 
 def export_scoreboard_dat() -> None:
-    nbt = nbtlib.load(os.path.join(Config.WORLD_DATA_DIR, 'scoreboard.dat'))
-    jsnbt = json.dumps(nbt[""])
+    scoreboardJson: dict = {}
+    if (Config.SCOREBOARDDAT_FILE not in Config.NONEXISTENT_FILES):
+        nbt = nbtlib.load(os.path.join(Config.SCOREBOARDDAT_FILE))
+        scoreboardJson = nbt
     with open(os.path.join(Config.OUTPUT_DIR, "scoreboard.json"), 'w') as f:
-        f.write(jsnbt)
+        f.write(json.dumps(scoreboardJson))
 
 
 def export_villages_dat() -> None:
     villagesJson: dict = {}
-    nbt = nbtlib.load(os.path.join(Config.WORLD_DATA_DIR, 'villages.dat'))
-    villagesJson['overworld'] = nbt[""]
-    nbt = nbtlib.load(
-        os.path.join(Config.WORLD_DATA_DIR, 'villages_nether.dat'))
-    villagesJson['nether'] = nbt[""]
-    nbt = nbtlib.load(os.path.join(Config.WORLD_DATA_DIR, 'villages_end.dat'))
-    villagesJson['end'] = nbt[""]
+    if (Config.OVERWORLD_VILLAGE_FILE not in Config.NONEXISTENT_FILES):
+        nbt = nbtlib.load(Config.OVERWORLD_VILLAGE_FILE)
+        villagesJson['overworld'] = nbt
+    if (Config.NETHER_VILLAGE_FILE not in Config.NONEXISTENT_FILES):
+        nbt = nbtlib.load(Config.NETHER_VILLAGE_FILE)
+        villagesJson['nether'] = nbt
+    if (Config.END_VILLAGE_FILE not in Config.NONEXISTENT_FILES):
+        nbt = nbtlib.load(Config.END_VILLAGE_FILE)
+        villagesJson['end'] = nbt
     with open(os.path.join(Config.OUTPUT_DIR, 'villages.json'), 'w') as f:
         f.write(json.dumps(villagesJson))
